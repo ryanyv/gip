2b32da5a8491cdde76725943e1a77d1e3b4b29c5
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-22 21:57:03 +0200

    Improve edit profile layout

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm
 from .models import User
 
 class UserUpdateForm(forms.ModelForm):
@@ -19,5 +18,5 @@ class UserUpdateForm(forms.ModelForm):
             'username': forms.TextInput(attrs={'class': 'appearance-none block w-full px-3 py-2 bg-[#1f1f1f] border border-gold rounded-md text-white focus:outline-none focus:ring-gold focus:border-gold sm:text-sm'}),
             'email': forms.EmailInput(attrs={'class': 'appearance-none block w-full px-3 py-2 bg-[#1f1f1f] border border-gold rounded-md text-white focus:outline-none focus:ring-gold focus:border-gold sm:text-sm'}),
             'phone_number': forms.TextInput(attrs={'class': 'appearance-none block w-full px-3 py-2 bg-[#1f1f1f] border border-gold rounded-md text-white focus:outline-none focus:ring-gold focus:border-gold sm:text-sm'}),
-            'profile_photo': forms.ClearableFileInput(attrs={'class': 'block w-full text-sm text-white border border-gold rounded-md cursor-pointer bg-[#1f1f1f] focus:outline-none'}),
+            'profile_photo': forms.FileInput(attrs={'class': 'hidden', 'accept': 'image/*'}),
         }
