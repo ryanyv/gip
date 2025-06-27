369440134a0cdc506b4c3bbede5b9b6a048f4aee
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-27 20:54:10 +0200

    Link home page to real properties

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from properties.models import Property
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+    def test_home_displays_featured_property(self):
+        Property.objects.create(
+            name="Home Test Property",
+            property_type="short-term",
+            location="Test Location",
+            description="desc",
+            is_featured=True,
+        )
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Home Test Property")
+
