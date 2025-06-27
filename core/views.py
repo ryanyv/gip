94f7a3bee4221eb951090e041397ce99d125a853
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-27 20:54:07 +0200

    Link home page properties to database

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from properties.models import Property
 
 def home(request):
-    return render(request, "core/home.html")
+    featured_properties = (
+        Property.objects.filter(is_featured=True).order_by("-created_at")[:3]
+    )
+    return render(
+        request,
+        "core/home.html",
+        {
+            "featured_properties": featured_properties,
+        },
+    )
 
 def about(request):
     return render(request, "core/about.html")
