0e98f5654698729ebad82e07fec02108f884e72a
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-28 14:15:23 +0200

    Customize admin property list

--- properties/admin.py
+++ properties/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Facility, Property, Photo, Booking
 
 class PhotoInline(admin.TabularInline):
@@ -9,7 +10,9 @@ class PhotoInline(admin.TabularInline):
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
+    change_list_template = "admin/properties/property/change_list.html"
     list_display = (
+        'thumbnail',
         'name',
         'responsible',
         'property_type',
@@ -21,10 +24,21 @@ class PropertyAdmin(admin.ModelAdmin):
         'is_featured',
         'created_at',
     )
+    list_display_links = None
     list_filter = ('property_type', 'is_featured', 'guests')
     search_fields = ('name', 'location')
     inlines = [PhotoInline]
     filter_horizontal = ('facilities',)
+
+    @admin.display(description="Photo")
+    def thumbnail(self, obj):
+        first_photo = obj.photos.first()
+        if first_photo:
+            return format_html(
+                '<img src="{}" width="60" height="45" style="object-fit:cover;"/>',
+                first_photo.image.url,
+            )
+        return ""
 
 @admin.register(Facility)
 class FacilityAdmin(admin.ModelAdmin):
