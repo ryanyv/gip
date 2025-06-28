6608feb035007c78678f6027e0fdec7500a42aa1
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-28 14:02:55 +0200

    Add custom properties management page

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='admin_panel_dashboard'),
+    path('properties/', views.manage_properties, name='admin_panel_properties'),
 ]
