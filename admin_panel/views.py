9b4e35935ab11be0df991ae7ed7b7e0a81bf0a8a
ryanyv <71137018+ryanyv@users.noreply.github.com>
2025-06-28 17:34:54 +0200

    Add bookings management page

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from accounts.models import User
-from properties.models import Property, Booking
+from properties.models import Property, Booking, PROPERTY_TYPE_CHOICES
 
 def is_admin_or_superadmin(user):
     """Return True if the user has admin privileges."""
@@ -48,3 +48,21 @@ def delete_property(request, pk):
     prop = get_object_or_404(Property, pk=pk)
     prop.delete()
     return JsonResponse({"deleted": True})
+
+
+@login_required
+@user_passes_test(is_admin_or_superadmin)
+def manage_bookings(request):
+    """List properties for booking management."""
+    filter_type = request.GET.get("type", "short-term")
+    properties = Property.objects.all()
+    valid_types = [choice[0] for choice in PROPERTY_TYPE_CHOICES]
+    if filter_type in valid_types:
+        properties = properties.filter(property_type=filter_type)
+    properties = properties.order_by("-created_at")
+    context = {
+        "properties": properties,
+        "filter_type": filter_type,
+        "types": PROPERTY_TYPE_CHOICES,
+    }
+    return render(request, "admin_panel/manage_bookings.html", context)
